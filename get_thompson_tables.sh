3b85b7bc6fb381d79cb042bf673baca2511898fb
Ted Mansell <ted.mansell@noaa.gov>
2024-01-11 11:45:04 -0600

    first commit

---
+++ get_thompson_tables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script stolen from the SCM contrib directory
+
+set -ex
+
+if [[ $(uname -s) == Darwin ]]; then
+  if [[ $(sw_vers -productVersion) < 12.3 ]]; then
+    MYDIR=$(cd "$(dirname "$(greadlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+  else
+    MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+  fi
+else
+  MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+fi
+BASEDIR=$MYDIR/..
+
+# Change to directory containing the physics input data, download and extract archive
+# cd $BASEDIR/scm/data/physics_input_data/
+wget https://github.com/NCAR/ccpp-scm/releases/download/v6.0.0/thompson_tables.tar.gz
+tar -xvf thompson_tables.tar.gz
+rm -f thompson_tables.tar.gz
+# cd $BASEDIR/
+
